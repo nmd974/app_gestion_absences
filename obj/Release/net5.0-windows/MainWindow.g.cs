26789691a0fe00c05f7a682dce1cbd260c2c5410
Gilles-boyer <boyer.gilles@live.fr>
2022-03-09 15:18:11 +0400

    ajout bdd

--- obj/Release/net5.0-windows/MainWindow.g.cs
+++ obj/Release/net5.0-windows/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace GestionAbsences {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GestionAbsences;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GestionAbsences_z2rr3agw_wpftmp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
