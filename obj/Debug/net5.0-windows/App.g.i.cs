48bdee9189fc0700f031a6726a9458c5e083f315
nmd974 <jha.payet@gmail.com>
2022-03-10 09:23:28 +0400

    Ajout de material design package

--- obj/Debug/net5.0-windows/App.g.i.cs
+++ obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace GestionAbsences {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.14.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace GestionAbsences {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.14.0")]
         public static void Main() {
             GestionAbsences.App app = new GestionAbsences.App();
             app.InitializeComponent();
